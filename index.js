8b26a6af7f6111f7009790248884a57686aafba4
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-08-26 15:43:54 +0100

    master

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
 const score = document.getElementById('score')
+let squares = []
+
 
 function createGrid(){
-    
+    for (let i=0 ; i < 100 ; i++){
+
+    const square = document.createElement('div')
+
+    square.classList.add('square')
+
+    grid.appendChild(square)
+
+        
+
+    }
+
 }
+createGrid()
