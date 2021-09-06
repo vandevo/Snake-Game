9931ba0d6fc3988555eed7f8a55c1de8e1aff422
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-09-06 14:28:37 +0100

    master

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const score = document.getElementById('score')
 let squares = []
 let currentSnake = [2,1,0]
 let direction = 1
+const width = 10
 
 function createGrid(){
-    for (let i=0 ; i < 100 ; i++){
+    for (let i=0 ; i < width*width ; i++){
 
     const square = document.createElement('div')
 
@@ -24,6 +25,19 @@ createGrid()
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
 function move(){
+    
+    if(
+        (currentSnake[0] + width >= width*width && direction === width) ||
+        (currentSnake[0] % width === width-1 && direction  === 1) ||
+        (currentSnake[0] % width === 0 && direction  === -1) ||
+        (currentSnake[0] - width < 0 && direction  === -width) ||
+        squares[currentSnake[0] + direction].classList.contains('snake')
+    )
+    return clearInterval(timerId)
+
+
+
+
     const tail = currentSnake.pop()
     squares[tail].classList.remove('snake')
     currentSnake.unshift(currentSnake[0] + direction)
@@ -32,7 +46,14 @@ function move(){
 move()
 
 let timerId = setInterval(move, 1000)
-// clearInterval(timerId)
+
+function generateApples(){
+    do {
+    appleIndex = Math.floor(Math.random() * squares.length)
+    } while (squares[appleIndex].classList.contains('snake'))
+    squares[appleIndex].classList.add('apple')
+}
+generateApples()
 
 // 39 is right arrow
 // 38 is up arrow 
@@ -42,11 +63,18 @@ let timerId = setInterval(move, 1000)
 function control(e) {
     if (e.keyCode === 39){
         console.log('right pressed')
+        direction = 1
     } else if (e.keyCode === 38){
         console.log('up pressed')
+        direction = -width
     } else if (e.keyCode === 37){
         console.log('left pressed')
+        direction = -1
     } else if (e.keyCode === 40){
         console.log('down pressed')
+        direction = +width
     }
 }
+
+document.addEventListener('keyup', control)
+document.addEventListener('keydown', control)
